ecfe0277768a1591c2db85dd37e8e7bfab1edb82
Matthew Stevens <mjstevens777@gmail.com>
2015-10-28 17:02:11 -0700

    Fixed random seeding and gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include "stereo-dataset.h"
 #include "opencv2/highgui/highgui.hpp"
+#include <cstdlib>
+#include <ctime>
 
 #define WINDOW_NAME "Stereo Pair"
 
 int main(int argc, const char *argv[]) {
   StereoDataset dataset;
+  srand (time(NULL));
+
   StereoPair pair = dataset.get_random_stereo_pair();
 
   cv::namedWindow("Stereo Pair", cv::WINDOW_AUTOSIZE);
