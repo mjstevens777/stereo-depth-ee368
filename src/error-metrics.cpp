2556d69effead526ce344276f5b5ca037c2ba3dd
zliu24 <lucasliu94s@gmail.com>
2015-11-09 01:02:03 -0800

    Adding remaining files for error metrics

--- src/error-metrics.cpp
+++ src/error-metrics.cpp
@@ -1,6 +1,27 @@
-/*
-  rms of non-occluded regions
-  rms of textured regions
+#include "error-metrics.h"
+#include <math.h>
+#include <iostream>
 
-  accuracy of occluded vs. non-occluded classification
-*/
+using namespace cv;
+
+double ErrorMetrics::get_rms_error_all (const Mat gold_disparity, const Mat guess_disparity) {
+	int num_pixel = gold_disparity.rows * gold_disparity.cols;
+
+	// sqrt(1/num_pixel*sum(diff^2))
+	Mat diff = gold_disparity - guess_disparity;
+	double score = 1/sqrt(num_pixel)* norm(diff,NORM_L2);
+
+	return score;
+}
+
+double ErrorMetrics::get_bad_matching_all (const Mat gold_disparity, const Mat guess_disparity)  {
+	int num_pixel = gold_disparity.rows * gold_disparity.cols;
+
+	// sqrt(1/num_pixel*sum( abs(diff) > thresh))
+	Mat abs_diff = abs(gold_disparity - guess_disparity);
+	Mat is_bad = (abs_diff > EVAL_BAD_THRESH) / 255;
+	double num_bad_pixel = sum(is_bad)[0];
+	double score = num_bad_pixel/num_pixel;
+
+	return score;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include "stereo-dataset.h"
 #include "opencv2/highgui/highgui.hpp"
+#include "error-metrics.h"
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
+
+using namespace std;
 
 #define WINDOW_NAME "Stereo Pair"
 
@@ -11,9 +15,23 @@ int main(int argc, const char *argv[]) {
 
   StereoPair pair = dataset.get_random_stereo_pair();
 
+  //Test rms
+  double true_rms = ErrorMetrics::get_rms_error_all(pair.true_disparity_left,  pair.true_disparity_left); 
+  double left_right_rms = ErrorMetrics::get_rms_error_all(pair.true_disparity_left,  pair.true_disparity_right); 
+  cout << "True rms: " << true_rms << "\n";
+  cout << "Left right rms: " << left_right_rms << "\n";
+
+  //Test bad matching
+  double true_bm = ErrorMetrics::get_bad_matching_all(pair.true_disparity_left,  pair.true_disparity_left); 
+  double left_right_bm = ErrorMetrics::get_bad_matching_all(pair.true_disparity_left,  pair.true_disparity_right); 
+  cout << "True bm: " << true_bm << "\n";
+  cout << "Left right bm: " << left_right_bm << "\n";
+
+
   cv::namedWindow("Stereo Pair", cv::WINDOW_AUTOSIZE);
   cv::imshow("Stereo Pair", pair.left);
 
   cv::waitKey(0); 
+
   return 0;
 }
